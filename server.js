75749b5e0893d9b3431e2a7e23166f07459ec7e8
kait14a12@gmail.com <kaito14a12@gmail.com>
2022-07-26 22:12:47 +0700

    update PORT using env var or default

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ require('dotenv').config()
 app.use(express.json());
 
 app.use('/api', rootRouter)
+const PORT =  process.env.PORT || 3000
 
 app.listen(3000, () => {
   console.log("server is running!!")
